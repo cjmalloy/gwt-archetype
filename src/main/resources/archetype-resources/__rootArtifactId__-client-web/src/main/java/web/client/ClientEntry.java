a2fbbc3a4c0f13d138137e51da53a3ef7f79fb75
Chris Malloy <cjmalloy@gmail.com>
2016-03-05 18:15:23 -0400

    Initial commit

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-client-web/src/main/java/web/client/ClientEntry.java
@@ -0,0 +1,11 @@
+package ${package}.web.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class ClientEntry implements EntryPoint  {
+
+  @Override
+  public void onModuleLoad() {
+
+  }
+}
